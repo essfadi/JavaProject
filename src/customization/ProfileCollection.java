77ef7a41986fb5d64eddbb16842e47a4c7792fc1
HibaJo <69761639+HibaJo@users.noreply.github.com>
2021-12-08 00:54:30 +0100

    ProfileCollection.java
    Change case1 and add cases 8,9,10.

--- src/customization/ProfileCollection.java
+++ src/customization/ProfileCollection.java
@@ -7,8 +7,13 @@ package customization;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashSet;
 import java.util.Iterator;
 import java.util.Scanner;
+import java.util.ListIterator;
+import java.util.Set;
+import platform.component.*;
+import customization.*;
 
 /**
  *
@@ -49,13 +54,12 @@ public class ProfileCollection {
     public void modifyProfile(String name) {
         Profile placeHolder = this.searchProfile(name);
         Scanner scanner = new Scanner(System.in);
-        int playback_choice;
         int choice_change;
         if (placeHolder != null) {
             System.out.println("1. Name.\n2. Level of restriction.\n3. email."
                     + "\n4. Notifications.\n5. Language.\n6. Playback Option."
                     + "\n7. Subtitle Activation.\n8. Favorite Shows.\n"
-                    + "9. Blocked Shows.\n8. Subtitle Language.\n");
+                    + "9. Blocked Shows.\n10. Subtitle Language.\n");
             System.out.print("Choose The Element You want to change: ");
             choice_change = scanner.nextInt();
             switch (choice_change) {
@@ -65,6 +69,10 @@ public class ProfileCollection {
                     placeHolder.setName(newname);
                     break;
                 case 2:
+                    System.out.println("Please enter the minimal age for this profile:");
+                    int age = scanner.nextInt();
+                    MaturityLevel level = new MaturityLevel(age);
+                    placeHolder.setLevel_restriction(level);
                     break;
                 case 3:
                     System.out.print("Please enter the new email: ");
@@ -72,8 +80,13 @@ public class ProfileCollection {
                     placeHolder.setEmail(email);
                     break;
                 case 4:
-                    System.out.println("");
+                    System.out.println("Your Notification status has been changed!");
                     placeHolder.setToNotify(!placeHolder.isToNotify());
+                    if (placeHolder.isToNotify()) {
+                        System.out.println("Notifications are activated!");
+                    } else {
+                        System.out.println("Notifications are disactivted!");
+                    }
                     break;
                 case 5:
                     System.out.print("Please enter the new language: ");
@@ -81,35 +94,54 @@ public class ProfileCollection {
                     placeHolder.setLanguage(language);
                     break;
                 case 6:
-                    System.out.println("Please enter your playback Settings.\n1. AUTO \n2. LOW \n3. MEDIUM \n4. HIGH");
-                    do {
-                        System.out.print("Chooose a Play Back setting: ");
-                        playback_choice = scanner.nextInt();
-                    } while (!(playback_choice < 5 && playback_choice > 0));
-
-                    switch (playback_choice) {
-                        case 1:
-                            Playback setting = Playback.AUTO;
-                            break;
-                        case 2:
-                            setting = Playback.LOW;
-                            break;
-                        case 3:
-                            setting = Playback.MEDIUM;
-                            break;
-                        default:
-                            setting = Playback.HIGH;
-                            break;
-                    }
+                    System.out.print("Please enter your playback Settings.\n1. AUTO \n2. LOW \n3. MEDIUM \n4. HIGH");
+                    String playBack = scanner.nextLine();
+                    placeHolder.setPlayback(Playback.valueOf(playBack.toUpperCase()));
                     break;
                 case 7:
-                    System.out.print("Please enter something to toggle the activation status:");
-                    scanner.next();
-                    
+                    System.out.print("Please enter a space to toggle the activation status:");
+                    String click = scanner.nextLine();
+                    placeHolder.setSubtitles(!placeHolder.isSubtitles());
                     break;
                 case 8:
+                    System.out.print("Please enter a show that you would like to remove from Favorites: ");
+                    String title = scanner.nextLine();
+                    //Search for show by name
+                    if (placeHolder.getFavorites() == null) {
+                        System.out.println("You have no show in Favorites List");
+                    } else {
+                        ListIterator<Show> iter = placeHolder.getFavorites().listIterator();
+                        while (iter.hasNext()) {
+                            Show s = iter.next();
+                            if (title.equalsIgnoreCase(s.getTitle())) {
+                                placeHolder.getFavorites().remove(s);
+                                System.out.println(placeHolder.getFavorites());
+                            }
+                        }
+                        System.out.println("The show" + title + "you are looking for does not exist in Favorites List");
+                    }
                     break;
                 case 9:
+                    System.out.print("Please enter the title of the show to remove from the blocked list:");
+                    title = scanner.nextLine();
+                    if (placeHolder.getBlocked() == null) {
+                        System.out.println("Your Blocked List is Empty!");
+                    } else {
+                        ListIterator<Show> iter = placeHolder.getBlocked().listIterator();
+                        while (iter.hasNext()) {
+                            Show b = iter.next();
+                            if (title.equals(b.getTitle())) {
+                                placeHolder.getBlocked().remove(b);
+                            }
+                        }
+                        System.out.println("The show" + title + "you are looking for does not exist in Blocked List");
+                    }
+                    break;
+                case 10:
+                    System.out.println("Please enter the new language of subtitles");
+                    String subtitle = scanner.nextLine();
+                    placeHolder.setSubtitle_lang(Language.valueOf(subtitle.toUpperCase()));
+
                     break;
                 default:
                     System.out.println("The Choice You Entered Is Not Valid!");
