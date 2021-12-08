8de62bfb7b9fd161f9f51aca2346b9c56de6a452
Oussama Essfadi <oussamaessfadi@gmail.com>
2021-12-08 03:36:11 +0100

    Viewing collection is created - toString() needed, no modification required

---
+++ src/platform/component/ViewingCollection.java
@@ -0,0 +1,58 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package platform.component;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Iterator;
+/**
+ *
+ * @author oessf
+ */
+public class ViewingCollection {
+    private ArrayList<Viewing> viewings;
+    
+    public ViewingCollection() {
+        viewings = new ArrayList();
+    }
+
+    public ArrayList<Viewing> getViewings() {
+        return viewings;
+    }
+
+    public void setViewings(ArrayList<Viewing> viewings) {
+        this.viewings = viewings;
+    }
+    
+    public void add(Viewing vue) {
+        viewings.add(vue);
+    }
+    public Viewing search(String titleShow) {
+        Iterator<Viewing> iterator = viewings.iterator();
+        while(iterator.hasNext()) {
+            Viewing vue = iterator.next();
+            if (vue.getShow().getTitle().equals(titleShow)) {
+                return vue;
+            }
+        }
+        return null;
+    }
+    public void remove(String titleShow) {
+        Viewing vue = this.search(titleShow);
+        if (vue != null) {
+            viewings.remove(vue);
+        }
+        else {
+            System.out.println("The Show you are trying to delete from viewing activity doesn't exist!");
+        }
+    }
+    public void sort() {
+        Collections.sort(viewings, new sortByDate());
+    }
+    @Override
+    public String toString() {
+        return "";
+    }
+}
---
+++ src/platform/component/sortByDate.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package platform.component;
+import java.util.Comparator;
+/**
+ *
+ * @author oessf
+ */
+public class sortByDate implements Comparator<Viewing>{
+    @Override
+    public int compare(Viewing a, Viewing b) {
+        return a.getDate().compareTo(b.getDate());
+    }
+}
