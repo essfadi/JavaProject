cb531bc6447c701d32f565d4d27e1dfdb12938a9
HibaJo <69761639+HibaJo@users.noreply.github.com>
2021-12-12 18:49:35 +0100

    Update Netflix.java

--- src/main/Netflix.java
+++ src/main/Netflix.java
@@ -22,7 +22,7 @@ import platform.component.ShowCollection;
 
 public class Netflix {
 
-    public static  ShowCollection shows; // Array or List
+    public static ShowCollection shows;
 
     private AccountCollection accList;
 
@@ -34,6 +34,9 @@ public class Netflix {
     private final Map<String, String> maturityByCountry = new HashMap<>(); // Regions Selector or enum
 
     public Netflix() {
+        shows = new ShowCollection();
+        accList = new AccountCollection();
+        requests = new RequestCollection();
     }
 
     Netflix(ShowCollection showList, Map plans_by_country, Map maturity_by_region) {
@@ -108,8 +111,7 @@ public class Netflix {
         password = scanner.next();
         System.out.println("======================================================");
         Account acc = new Account(email, password);
-        return(accList.add(acc));
-        
+        return (accList.add(acc));
     }
 
     public void search(int choice, String data) {
@@ -150,8 +152,9 @@ public class Netflix {
             Request request = new Request(fake, title);
             System.out.println("Your request has been sent successfully!\n Click '1' to see your request info.");
             int choice_request = scanner.nextInt();
-            if(choice_request==1)
+            if (choice_request == 1) {
                 System.out.println(request.toString());
+            }
         }
     }
 
@@ -290,11 +293,7 @@ public class Netflix {
         synopsis = scanner.nextLine();
         System.out.print("Enter the minimum age allowed to watch: "); // Should be a switch (not sure)
         age = scanner.nextInt();
-        if (age > 0) {
-            levels = new MaturityLevel(age);
-        } else {
-            throw new AgeException("\tThe Age You Entered Is Not Valid");
-        }
+        levels = setMaturityLevel(age);
         System.out.println("======================================================");
         System.out.print("********Show Menu*********\n1/Movie\n2/Serie\nPlease Enter your Choice:");
         do {
@@ -316,4 +315,21 @@ public class Netflix {
         return (shows);
     }
 
+    public MaturityLevel setMaturityLevel(int age) throws AgeException {
+        if (age > 0) {
+            if (age < 7) {
+                return (MaturityLevel.ALL);
+            } else if (age < 13) {
+                return (MaturityLevel.KIDS);
+            } else if (age < 16) {
+                return (MaturityLevel.TEENS);
+            } else if (age < 18) {
+                return (MaturityLevel.ADULT);
+            } else {
+                return (MaturityLevel.ADULTS);
+            }
+        } else {
+            throw new AgeException("\tThe Age You Entered Is Not Valid");
+        }
+    }
 }
